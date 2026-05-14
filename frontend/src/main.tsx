ad5057116d9895481cf92a98b4813cdffdf7379c
Jinyws <jinyws@gmail.com>
2026-05-14 12:28:02 +0800

    Switch frontend entry to stable app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import StableApp from './StableApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <StableApp />
   </React.StrictMode>
 )
