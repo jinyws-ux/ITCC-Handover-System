117beb8564f9911dd91622381572f5f88a004532
Jinyws <jinyws@gmail.com>
2026-05-14 13:14:18 +0800

    Switch frontend entry to AppV3

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AppV2 from './AppV2'
+import AppV3 from './AppV3'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AppV2 />
+    <AppV3 />
   </React.StrictMode>
 )
