8035fcfb258b14d79d2eba4b7c011b15511c4205
Jinyws <jinyws@gmail.com>
2026-05-14 11:08:59 +0800

    Add Tailwind config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        ink: '#111827'
+      }
+    }
+  },
+  plugins: []
+}
